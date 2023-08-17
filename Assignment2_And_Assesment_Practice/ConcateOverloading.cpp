f57681289801b3f9110327cdcaffb3cd1458cb10
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-08-17 20:00:36 +0530

    New files

--- Assignment2_And_Assesment_Practice/ConcateOverloading.cpp
+++ Assignment2_And_Assesment_Practice/ConcateOverloading.cpp
@@ -1,11 +1,32 @@
-#include<iostream>
-using namespace std;
-class Concatenation
-{
-    string concat(string a, string b)
-    {
-        return a+b;
+#include <iostream>
+#include <string>
+
+class StringConcatenation {
+private:
+    std::string value;
+
+public:
+    StringConcatenation(const std::string& value) : value(value) {}
+
+    StringConcatenation& concat(const std::string& other) {
+        value += other;
+        return *this;
     }
 
-    
+    std::string getValue() const {
+        return value;
+    }
 };
+
+int main() {
+    StringConcatenation str1("Hello, ");
+    str1.concat("world!");
+
+    StringConcatenation str2("C++ is ");
+    str2.concat("awesome!");
+
+    std::cout << str1.getValue() << std::endl;
+    std::cout << str2.getValue() << std::endl;
+
+    return 0;
+}
