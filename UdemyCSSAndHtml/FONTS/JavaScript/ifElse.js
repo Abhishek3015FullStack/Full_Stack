c28366338c09fc5e0ad290358993cd934c95b6cf
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-20 09:05:57 +0530

    commit

--- UdemyCSSAndHtml/FONTS/JavaScript/ifElse.js
+++ UdemyCSSAndHtml/FONTS/JavaScript/ifElse.js
@@ -12,11 +12,39 @@ if(ScoredMarks<=35)
 {
     console.log("fail");
 }
-else if(ScoredMarks>=35 && ScoredMarks<=50)
+if(ScoredMarks>=35 && ScoredMarks<=50)
 {
     console.log("minimum");
 }
 else
 {
     console.log("pass");
+}
+
+var  number =5;
+if(number>0)
+{
+    if(number>=5)
+    {
+        //console.log("it is greater than the 5")
+        if(number>=5 && number<=10)
+        {
+            console.log("5 to 10")
+        }
+
+    }
+    else
+    {
+        if(number<5)
+        {
+            if(number <5 && number>1)
+            {
+                console.log("1 to 4");
+            }
+        }
+        else
+        {
+            console.log("no have the number");
+        }   
+    }
 }
