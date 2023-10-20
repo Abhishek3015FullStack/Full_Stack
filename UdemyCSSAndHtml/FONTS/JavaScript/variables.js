a366408f07f6dd0f4c31a50fe934e96d33177a99
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-20 05:30:37 +0530

    new changes

--- UdemyCSSAndHtml/FONTS/JavaScript/variables.js
+++ UdemyCSSAndHtml/FONTS/JavaScript/variables.js
@@ -1,4 +1,13 @@
-var name ="Abhishek";
+var name ="Abhishek" , age=25,a=true ,b=10.23;
 console.log(name);
+console.log(age);
+console.log(a);
+console.log(b);
 
-var age;
+var d=function(){};
+var e={};
+var f =[];
+
+console.log(d);
+console.log(e);
+console.log(f);
