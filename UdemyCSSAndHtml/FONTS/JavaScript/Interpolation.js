fa020748cc28a87a903e8a615ed2624da3709f17
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-20 08:16:23 +0530

    Abhishek

---
+++ UdemyCSSAndHtml/FONTS/JavaScript/Interpolation.js
@@ -0,0 +1,9 @@
+var name ="Ambe Mata";
+var age ="anadhi  kal";
+
+var c =`hello ${name}, and age is ${age}`;
+console.log(c);
+
+
+console.log(`hello ${name}, and age is ${age}`);
+
---
+++ UdemyCSSAndHtml/FONTS/JavaScript/ifElse.js
@@ -0,0 +1,22 @@
+var age=8;
+if(age>=9)
+{
+    console.log("matured");
+}
+else
+{
+    console.log("not mature");
+}
+var ScoredMarks = 50;
+if(ScoredMarks<=35)
+{
+    console.log("fail");
+}
+else if(ScoredMarks>=35 && ScoredMarks<=50)
+{
+    console.log("minimum");
+}
+else
+{
+    console.log("pass");
+}
