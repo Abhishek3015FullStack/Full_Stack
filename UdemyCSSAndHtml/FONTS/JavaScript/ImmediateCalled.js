82770fda1b8452fb07a0df834cc98bd3b7252db3
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-21 13:48:08 +0530

    iife

--- UdemyCSSAndHtml/FONTS/JavaScript/ImmediateCalled.js
+++ UdemyCSSAndHtml/FONTS/JavaScript/ImmediateCalled.js
@@ -4,3 +4,12 @@ function print(m)
 }
 
 print((function(){return 10*500})());
+
+
+
+function printmsg(value)
+{
+    console.log(`this is value of  the printmsg ${value}`);
+}
+
+printmsg((function(m,n,o){return m*n*o;})(10,10,20));
