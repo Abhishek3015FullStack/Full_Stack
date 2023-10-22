ba1611587d6419c7e8be0e50632c237f2b14ad24
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-22 15:49:09 +0530

    fd

---
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/Object_javaScript/Destructuring_The_Object.js
@@ -0,0 +1,16 @@
+var Student={name:"abhishek", age:25 , email:"abhishek@gmail.com", address:"sda", city:"dhule"};
+
+
+var{name:fn,age:age1,email:email1,...oter} =Student;// assigiing the value in this please check  adn ... operator 
+
+console.log(fn);
+console.log(age1);
+console.log(email1);
+console.log(oter);
+
+
+fn = "sadadada";
+
+console.log(fn);
+
+console.log(Student);
