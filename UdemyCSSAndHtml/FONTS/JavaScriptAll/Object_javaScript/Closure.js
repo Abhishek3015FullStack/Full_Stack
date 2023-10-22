b72bd271330e9c8af1200d61ed76b0b5c246e42e
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-22 17:00:03 +0530

    ds

---
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/Object_javaScript/Closure.js
@@ -0,0 +1,14 @@
+function bankDeposit()
+{
+     var currentBalance=100;
+    let deposit = function (DepositAmount)
+    {
+        currentBalance= currentBalance+DepositAmount;
+        return `the balance of the curremnt is ${currentBalance}`;
+    }
+    return deposit;
+}
+
+
+let ba = bankDeposit();
+console.log(ba(1000));
