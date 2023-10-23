191ff45f6b3ece8955f232463750e4ed929a67f4
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-23 05:40:57 +0530

    ds

--- UdemyCSSAndHtml/FONTS/JavaScriptAll/Object_javaScript/Closure.js
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/Object_javaScript/Closure.js
@@ -12,3 +12,41 @@ function bankDeposit()
 
 let ba = bankDeposit();
 console.log(ba(1000));
+
+//practice
+
+var subject = function()
+{
+    var fees =20;
+    var paid= function(paid_amount)
+    {
+        fees  = fees+paid_amount;
+        return fees;
+    }
+    var debate=function(debate_amount)
+    {
+        fees = fees-debate_amount;
+        return fees;
+    }
+    return {paid,debate};
+}
+
+let valid = subject();
+console.log(valid.paid(5));
+
+console.log(valid.debate(25));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
