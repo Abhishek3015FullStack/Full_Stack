7d3037ad1acbc93a05e309edb13131e96d6fc9a3
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-23 05:41:40 +0530

    ds

---
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/Object_javaScript/SelfInvoking.js
@@ -0,0 +1,24 @@
+var BankAccountFunction = function()
+{
+    var account_balance =100;
+    let deposit = function (deposit_amount)
+    {   
+        account_balance=account_balance+deposit_amount;
+        return `the account after deposit is ${account_balance}`;
+    }
+    let withdraw = function(withdraw_amount)
+    {
+        account_balance= account_balance-withdraw_amount;
+        return `after teh withdraw the balance is ${account_balance}`;
+    }
+    return {deposit,withdraw}
+
+}();
+
+
+
+console.log(BankAccountFunction.deposit(110));
+
+
+console.log(BankAccountFunction.deposit(1500));
+
