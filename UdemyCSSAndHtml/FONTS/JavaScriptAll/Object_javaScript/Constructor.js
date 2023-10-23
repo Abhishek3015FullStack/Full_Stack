785dc8cd3518f17de980ddf81436a19b621bd111
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-23 08:04:49 +0530

    ds

---
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/Object_javaScript/Constructor.js
@@ -0,0 +1,19 @@
+var Const=function(EmployeeName,salary)
+{
+    var Employee={};
+    Employee.EmployeeName=EmployeeName,
+    Employee.salary=salary,
+
+    EmployeeGetdata= function()
+
+    {
+        return `${this.Employee.EmployeeName} and salary is ${this.salary}`;
+    };
+    return Employee;
+};
+
+var E1 = Const("a",1);
+console.log(E1.EmployeeName);
+
+var E2 =Const("b",2);
+console.log(E2.EmployeeName);
