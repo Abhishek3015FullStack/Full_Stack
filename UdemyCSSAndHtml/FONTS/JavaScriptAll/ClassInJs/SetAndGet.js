9ea3cf0a66917485cf3e755966286b1f4ca19d75
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-10-30 08:20:36 +0530

    sa

---
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/ClassInJs/SetAndGet.js
@@ -0,0 +1,39 @@
+class Person
+{
+     #Person_name;
+     #Person_age;
+
+     constructor()
+     {
+        this.#Person_age = 10;
+        this.#Person_name ="Abhishek";
+     }
+     seData(name ,age )
+    {
+        if((name!="undefined" && name!="null"))
+        {
+            this.#Person_name=name;
+            this.#Person_age =age;
+
+        }
+        else
+        {
+            console.log("error is occured by the user input");
+        }
+    }
+
+    getDataName()
+    {
+        return this.#Person_name;
+    }
+    getDataAge()
+    {
+        return this.#Person_age;
+    }
+}
+
+var Person1 = new Person();
+var a  =Person1.getDataName();
+console.log(a);
+
+Person1.seData("undefined",-10);
