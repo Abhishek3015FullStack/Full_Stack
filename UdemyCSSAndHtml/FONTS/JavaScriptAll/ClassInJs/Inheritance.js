477b06a8f42e9a8a7a53b950b567a2b23b810cb8
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-11-01 08:20:59 +0530

    ds

--- UdemyCSSAndHtml/FONTS/JavaScriptAll/ClassInJs/Inheritance.js
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/ClassInJs/Inheritance.js
@@ -2,7 +2,7 @@ class Person
 {
     person_name;
     person_age;
-<<<<<<< HEAD
+
     Desingation1;
     constructor(name,age)
     {
@@ -55,26 +55,6 @@ var emp1 =new Employee("Abhsiehk");
 emp1.Age1 =10;
 console.log(emp1.Designation);
 
-
-    constructor()
-    {
-        this.person_name ="Abhishek";
-        this.person_age =10;
-
-    }
-    set person_name(name)
-    {
-        this.person_name =name;
-    }
-    set person_age(age)
-    {
-        this.person_age = age;
-    }
-    get NameAndAge()
-    {
-        return `${this.person_name} and ${this.person_age}`;
-    }
-}
 class Employee extends Person
 {
     designation;
@@ -104,4 +84,4 @@ var a   = new Employee();
 a.person_age =50;
 
 console.log(a.NameAndAge);
->>>>>>> d54bb8faf2df4ebf68643cc4088d71f0c9c38a06
+
