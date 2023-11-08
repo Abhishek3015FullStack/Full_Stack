bd9c489e3ddf7d636889c9af42f349fb272de106
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-11-08 19:18:45 +0530

    ds

--- UdemyCSSAndHtml/FONTS/JavaScriptAll/Arrays/Every.js
+++ UdemyCSSAndHtml/FONTS/JavaScriptAll/Arrays/Every.js
@@ -3,7 +3,7 @@ var a =
     {Student_name:"Abhishek1", age:251, postalcode:4240011},
     {Student_name:"Abhishek2", age:252,postalcode:424002},
     {Student_name:"Abhishek3", age:253,postalcode:424003},
-    {Student_name:"Abhishek4", age:0,postalcode:424004},
+    {Student_name:"Abhishek4", age:566,postalcode:424004},
 ]
 
 function Abhishek(d)
