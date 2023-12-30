bf537fe839d94147244874bc13ffd3777d308f19
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-12-30 14:38:07 +0530

    dsa

---
+++ .Net/ShowingTheProductByUsingModel/ShowingTheProductByUsingModel/Controllers/ItemsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShowingTheProductByUsingModel.Models;
+
+namespace ShowingTheProductByUsingModel.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+        public ActionResult Index()
+        {
+            List<Items> items1 = new List<Items>()
+            {
+                new Items(){ItemsId=1, ItemsName="Abhishek1",ItemsPrice=5251},
+                new Items(){ItemsId=2, ItemsName="Abhishek2",ItemsPrice=5252},
+                new Items(){ItemsId=3, ItemsName="Abhishek3",ItemsPrice=5253}
+
+            };
+            ViewBag.Products = items1;
+
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            List<Items> Products = new List<Items>()
+            {
+                new Items(){ItemsId=1,ItemsName="Abhishek1",ItemsPrice=5251},
+                new Items(){ItemsId=1,ItemsName="Abhishek1",ItemsPrice=5252},
+                new Items(){ItemsId=1,ItemsName="Abhishek1",ItemsPrice=5253}
+            };
+            Items matchingProduct = null;
+            foreach (var a in Products)
+            {
+                if (a.ItemsId == id)
+                {
+                    matchingProduct = a;
+                }
+            }
+            ViewBag.Macthing = matchingProduct;
+            return View();
+        }
+    }
+}
---
+++ .Net/ShowingTheProductByUsingModel/ShowingTheProductByUsingModel/Models/Items.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShowingTheProductByUsingModel.Models
+{
+    public class Items
+    {
+        public int ItemsId { get; set; }
+        public String ItemsName { get; set; }
+        public long ItemsPrice { get; set; }
+
+    }
+}
