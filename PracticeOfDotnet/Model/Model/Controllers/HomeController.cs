aa57d9e1d026d04bf57840fec64f8f24684e8fc0
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2023-12-31 14:53:23 +0530

    dsa

---
+++ PracticeOfDotnet/Model/Model/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Models;
+namespace Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Products> productsList = new List<Products>()
+            {
+                new Products(){ ProductId = 1, ProductName="Abhishek 1" ,ProductPrice = 12.35f},
+                new Products(){ ProductId = 2, ProductName="Abhishek 2" ,ProductPrice = 12.35f},
+                new Products(){ ProductId = 3, ProductName="Abhishek 3" ,ProductPrice = 12.35f}
+
+            };
+            return View(productsList);
+        }
+        public ActionResult Details(int id)
+        {
+            List<Products> ProductList = new List<Products>()
+            {
+                new Products(){ ProductId = 1, ProductName="Abhishek 1" ,ProductPrice = 12.35f},
+                new Products(){ ProductId = 2, ProductName="Abhishek 2" ,ProductPrice = 12.35f},
+                new Products(){ ProductId = 3, ProductName="Abhishek 3" ,ProductPrice = 12.35f}
+            };
+            Products b = null;
+            foreach (var a in ProductList)
+            {
+                if (a.ProductId == id)
+                {
+                    b = a;
+                }
+            }
+            return View(b);
+        }
+    }
+}
---
+++ PracticeOfDotnet/Model/Model/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Model.Models
+{
+    public class Products
+    {
+        public int ProductId { set; get; }
+        public String ProductName { set; get; }
+        public float ProductPrice { set; get; }
+
+    }
+}
