ac3dc01f3b6172f4b82442e00db9fbc222f295d2
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2024-01-04 08:54:22 +0530

    ds

---
+++ .Net/NewRegardingDatabaseFetch/NewRegardingDatabaseFetch/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewRegardingDatabaseFetch.Models;
+
+namespace NewRegardingDatabaseFetch.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            AbhishekEntities1 db = new AbhishekEntities1();
+            List<Product> l1 =db.Products.ToList();
+            return View(l1);
+        }
+    }
+}
---
+++ .Net/ProductsList/ProductsList/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductsList.Models;
+
+namespace ProductsList.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Category> l1 = db.Categories.ToList();
+            return View(l1);
+        }
+    }
+}
