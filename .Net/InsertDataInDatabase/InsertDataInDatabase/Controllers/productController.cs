d2e29d52af965f716f281e714c4bc392f1cc9057
Abhishek3015FullStack <abhishekjain3015@gmail.com>
2024-01-08 13:17:53 +0530

    jh

---
+++ .Net/InsertDataInDatabase/InsertDataInDatabase/Controllers/productController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InsertDataInDatabase.Models;
+
+namespace InsertDataInDatabase.Controllers
+{
+    public class productController : Controller
+    {
+        // GET: product
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(Product p1)
+        {
+            ASDEntities1 a1 = new ASDEntities1();
+            a1.Products.Add(p1);
+            a1.SaveChanges();
+            return View();
+        }
+    }
+}
